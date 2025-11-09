ac665931360d549a6f9663ed3e37952cad7d5463
Pradeep-Selva-24 <143416835+Pradeep-Selva-24@users.noreply.github.com>
2025-11-09 22:22:43 +0530

    New changes

--- Controllers/FooterController.cs
+++ Controllers/FooterController.cs
@@ -4,16 +4,6 @@ namespace College.Controllers;
 
 public class FooterController(ILogger<HomeController> logger) : Controller
 {
-    public IActionResult Contact()
-    {
-        return View();
-    }
-
-    public IActionResult Feedback()
-    {
-        return View();
-    }
-
     public IActionResult PrivacyPolicy()
     {
         return View();
