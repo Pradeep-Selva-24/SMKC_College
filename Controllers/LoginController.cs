ce24a266d89a5ea4c9020dd55d3c0a69e466850d
puppet-coder <sirancheevi.palraj@gmail.com>
2026-01-11 13:17:07 +0530

    Login and admin panel added

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace College.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult login()
+        {
+            return View();
+        }
+    }
+}
